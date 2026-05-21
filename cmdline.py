4240eaf7b661d39a5f8d57dfd7256c537072d1c0
Jeff Glass <glass.jeffrey@gmail.com>
2026-05-21 08:53:43 -0500

    Update help message

--- cmdline.py
+++ cmdline.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser(
 )
 
 parser.add_argument("on_off", type=str.upper, choices=['ON', 'OFF'], help="Whether to turn the targeted device(s) on or off")
-parser.add_argument("--ip", required=True, type=ip_address, help="The IP address to send the message to")
+parser.add_argument("--ip", required=True, type=ip_address, help="The IP address to send the message to (AFH is 192.168.252.255)")
 
 if __name__ == "__main__":
     args = parser.parse_args()
